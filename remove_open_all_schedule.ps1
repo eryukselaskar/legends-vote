9a8e12594fa0860a481ad87e39a39e360ec630b4
Eryuksel <eryukselaskar@gmail.com>
2026-09-21 01:41:53 +0300

    Remove unused scheduler script

--- remove_open_all_schedule.ps1
+++
@@ -1,7 +0,0 @@
-$TaskName = "LegendsVoteOpenAll"
-try {
-    Unregister-ScheduledTask -TaskName $TaskName -Confirm:$false
-    Write-Host "Gorev kaldirildi: $TaskName"
-} catch {
-    Write-Host "Gorev bulunamadi: $_"
-}
